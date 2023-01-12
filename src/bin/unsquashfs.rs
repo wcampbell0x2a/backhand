ec99853e8fcd9efdbd6037e3c94eaaa2276f671b
wcampbell <wcampbell1995@gmail.com>
2023-01-12 17:56:58 -0500

    Improve and change unsquashfs add cli
    Improve and change `unsquashfs` and `add` cli to match `squashfs-tools/unsquashfs` cli

--- src/bin/unsquashfs.rs
+++ src/bin/unsquashfs.rs
@@ -7,38 +7,26 @@ use backhand::filesystem::{
     SquashfsSymlink,
 };
 use backhand::Squashfs;
-use clap::{Parser, Subcommand};
+use clap::Parser;
 
+/// tool to uncompress, extract and list squashfs filesystems
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version)]
 struct Args {
-    /// squashfs file
-    input: PathBuf,
+    /// Squashfs file
+    filesystem: PathBuf,
 
-    // Offset in file for Squashfs
-    #[arg(short, long, default_value_t = 0)]
+    /// Skip BYTES at the start of FILESYSTEM
+    #[arg(short, long, default_value_t = 0, name = "BYTES")]
     offset: u64,
 
-    #[command(subcommand)]
-    cmd: Command,
-}
+    /// List filesystem, do not write to DEST
+    #[arg(short, long)]
+    list: bool,
 
-#[derive(Subcommand, Debug)]
-pub enum Command {
-    ///// Extract single file from image
-    //ExtractFiles {
-    //    /// Filepath to extract
-    //    #[arg(short, long)]
-    //    filepath: Vec<String>,
-
-    //    #[arg(short, long, default_value = "output")]
-    //    output: PathBuf,
-    //},
-    /// Extract all files(Symlink/Files/Dirs) from image
-    ExtractAll {
-        #[arg(short, long, default_value = "output")]
-        output: PathBuf,
-    },
+    /// Extract to [PATHNAME]
+    #[arg(short, long, default_value = "squashfs-root", name = "PATHNAME")]
+    dest: PathBuf,
 }
 
 fn main() {
@@ -46,74 +34,66 @@ fn main() {
 
     let args = Args::parse();
 
-    match args.cmd {
-        //Command::ExtractFiles { filepath, output } => {
-        //    extract(&args.input, args.offset, filepath, &output)
-        //},
-        Command::ExtractAll { output } => extract_all(&args.input, args.offset, &output),
-    }
+    extract_all(&args);
 }
 
-fn extract_all(input: &Path, offset: u64, output: &Path) {
-    let file = File::open(input).unwrap();
-    let squashfs = Squashfs::from_reader_with_offset(file, offset).unwrap();
-    tracing::info!("SuperBlock: {:#02x?}", squashfs.superblock);
-    tracing::info!("Inodes: {:#02x?}", squashfs.inodes);
-    tracing::info!("Dirs: {:#02x?}", squashfs.all_dirs());
-    tracing::info!("Id: {:#02x?}", squashfs.id);
-    tracing::info!("Root inode: {:#02x?}", squashfs.root_inode);
-    tracing::info!("Fragments {:#02x?}", squashfs.fragments);
-
-    fs::create_dir_all(output).unwrap();
+fn extract_all(args: &Args) {
+    let file = File::open(&args.filesystem).unwrap();
+    let squashfs = Squashfs::from_reader_with_offset(file, args.offset).unwrap();
+    let _ = fs::create_dir_all(&args.dest);
     let filesystem = squashfs.into_filesystem().unwrap();
 
     for node in filesystem.nodes {
         let path = node.path;
-        match node.inner {
-            InnerNode::File(SquashfsFile { bytes, .. }) => {
-                let path: PathBuf = path.iter().skip(1).collect();
-                tracing::debug!("file {}", path.display());
-                let filepath = Path::new(output).join(path);
-                let _ = std::fs::create_dir_all(filepath.parent().unwrap());
-                match std::fs::write(&filepath, bytes) {
-                    Ok(_) => println!("[-] success, wrote {}", filepath.display()),
-                    Err(e) => {
-                        println!("[!] failed write: {} : {e}", filepath.display())
-                    },
-                }
-            },
-            InnerNode::Symlink(SquashfsSymlink { link, .. }) => {
-                let path: PathBuf = path.iter().skip(1).collect();
-                tracing::debug!("symlink {} {}", path.display(), link);
-                let filepath = Path::new(output).join(path);
-                let _ = std::fs::create_dir_all(filepath.parent().unwrap());
-                if std::os::unix::fs::symlink(&link, &filepath).is_ok() {
-                    println!("[-] success, wrote {}->{link}", filepath.display());
-                } else {
-                    println!("[!] failed write: {}->{link}", filepath.display());
-                }
-            },
-            InnerNode::Path(SquashfsPath { header, .. }) => {
-                let path: PathBuf = path.iter().skip(1).collect();
-                let path = Path::new(output).join(&path);
-                tracing::debug!("path {}", path.display());
-                let _ = std::fs::create_dir_all(&path);
-                let perms = Permissions::from_mode(u32::from(header.permissions));
-                fs::set_permissions(&path, perms).unwrap();
-                println!("[-] success, wrote {}", &path.display());
-            },
-            InnerNode::CharacterDevice(SquashfsCharacterDevice {
-                header: _,
-                device_number: _,
-            }) => {
-                println!("[-] character device not supported");
-            },
-            InnerNode::BlockDevice(SquashfsBlockDevice {
-                header: _,
-                device_number: _,
-            }) => {
-                println!("[-] block device not supported");
-            },
+        if !args.list {
+            match node.inner {
+                InnerNode::File(SquashfsFile { bytes, .. }) => {
+                    let path: PathBuf = path.iter().skip(1).collect();
+                    tracing::debug!("file {}", path.display());
+                    let filepath = Path::new(&args.dest).join(path);
+                    let _ = std::fs::create_dir_all(filepath.parent().unwrap());
+                    match std::fs::write(&filepath, bytes) {
+                        Ok(_) => println!("[-] success, wrote {}", filepath.display()),
+                        Err(e) => {
+                            println!("[!] failed write: {} : {e}", filepath.display())
+                        },
+                    }
+                },
+                InnerNode::Symlink(SquashfsSymlink { link, .. }) => {
+                    let path: PathBuf = path.iter().skip(1).collect();
+                    tracing::debug!("symlink {} {}", path.display(), link);
+                    let filepath = Path::new(&args.dest).join(path);
+                    let _ = std::fs::create_dir_all(filepath.parent().unwrap());
+                    if std::os::unix::fs::symlink(&link, &filepath).is_ok() {
+                        println!("[-] success, wrote {}->{link}", filepath.display());
+                    } else {
+                        println!("[!] failed write: {}->{link}", filepath.display());
+                    }
+                },
+                InnerNode::Path(SquashfsPath { header, .. }) => {
+                    let path: PathBuf = path.iter().skip(1).collect();
+                    let path = Path::new(&args.dest).join(&path);
+                    tracing::debug!("path {}", path.display());
+                    let _ = std::fs::create_dir_all(&path);
+                    let perms = Permissions::from_mode(u32::from(header.permissions));
+                    fs::set_permissions(&path, perms).unwrap();
+                    println!("[-] success, wrote {}", &path.display());
+                },
+                InnerNode::CharacterDevice(SquashfsCharacterDevice {
+                    header: _,
+                    device_number: _,
+                }) => {
+                    println!("[-] character device not supported");
+                },
+                InnerNode::BlockDevice(SquashfsBlockDevice {
+                    header: _,
+                    device_number: _,
+                }) => {
+                    println!("[-] block device not supported");
+                },
+            }
+        } else {
+            println!("{}", path.display());
         }
     }
 }

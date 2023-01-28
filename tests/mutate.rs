00794c6efa1b4f95bc1f91d94518946d2b1f9a1d
wcampbell <wcampbell1995@gmail.com>
2023-01-28 13:27:11 -0500

    Fix mutate test
    This test was just comparing against itself before, add a control
    squashfs that makes sure that the image was mutated.

--- tests/mutate.rs
+++ tests/mutate.rs
@@ -1,4 +1,5 @@
 mod common;
+use std::cell::RefCell;
 use std::fs::{self, File};
 use std::io::Cursor;
 
@@ -42,6 +43,11 @@ fn test_add_00() {
             hash: "dc02848152d42b331fa0540000f68bf0942c5b00a3a44a3a6f208af34b4b6ec3".to_string(),
             url: "wcampbell.dev/squashfs/testing/test_add_00/new.squashfs".to_string(),
         },
+        TestAssetDef {
+            filename: "control.squashfs".to_string(),
+            hash: "a227c214be3efbd9b6958918e23d13f4c98de7a1fde64c2a5ede1c4c69938930".to_string(),
+            url: "wcampbell.dev/squashfs/testing/test_add_00/control.squashfs".to_string(),
+        },
     ];
     const TEST_PATH: &str = "test-assets/test_add_00";
     let og_path = format!("{TEST_PATH}/out.squashfs");
@@ -52,35 +58,34 @@ fn test_add_00() {
     let og_filesystem = FilesystemReader::from_reader(file).unwrap();
     let mut new_filesystem = FilesystemWriter::from_fs_reader(&og_filesystem).unwrap();
 
+    let h = FilesystemHeader {
+        permissions: 0o755,
+        uid: 0,
+        gid: 0,
+        mtime: 0,
+    };
+
     // Add file
     let bytes = Cursor::new(b"this is a new file, wowo!");
     new_filesystem
-        .push_file(bytes, "a/d/e/new_file", FilesystemHeader::default())
+        .push_file(bytes, "a/d/e/new_file", h)
         .unwrap();
     // Add file
     new_filesystem
-        .push_file(
-            Cursor::new("i am (g)root"),
-            "root_file",
-            FilesystemHeader::default(),
-        )
+        .push_file(Cursor::new("i am (g)root"), "root_file", h)
         .unwrap();
     // Add file
     new_filesystem
-        .push_file(
-            Cursor::new("dude"),
-            "a/b/c/d/dude",
-            FilesystemHeader::default(),
-        )
+        .push_file(Cursor::new("dude"), "a/b/c/d/dude", h)
         .unwrap();
 
     // Modify file
-    //let file = new_filesystem.mut_file("/a/b/c/d/e/first_file").unwrap();
-    //file.bytes = b"MODIFIEDfirst file!\n".to_vec();
+    let file = new_filesystem.mut_file("/a/b/c/d/e/first_file").unwrap();
+    file.reader = RefCell::new(Box::new(Cursor::new(b"MODIFIEDfirst file!\n")));
 
     let bytes = new_filesystem.to_bytes().unwrap();
-    fs::write(new_path, bytes).unwrap();
+    fs::write(&new_path, bytes).unwrap();
 
-    let new_path = format!("{TEST_PATH}/new.squashfs");
-    test_unsquashfs(&new_path, &new_path, None);
+    let control_new_path = format!("{TEST_PATH}/control.squashfs");
+    test_unsquashfs(&new_path, &control_new_path, None);
 }

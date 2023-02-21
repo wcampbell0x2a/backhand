ce769f43d54dd26daff32ea74202212eb442d73b
wcampbell <wcampbell1995@gmail.com>
2023-02-20 20:31:55 -0500

    Release v0.10.0

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! Add the following to your `Cargo.toml` file:
 //! ```toml
 //! [dependencies]
-//! backhand = "0.9.1"
+//! backhand = "0.10.0"
 //! ```
 //!
 //! For reading an image and extracting its details and contents, use

1eba2cbbcc28670ecc560579aaa9d4df041bbd06
wcampbell <wcampbell1995@gmail.com>
2023-05-11 00:31:56 -0400

    Add test for unsquashfs/replace

---
+++ tests/replace.rs
@@ -0,0 +1,96 @@
+mod bin;
+mod common;
+
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use tempfile::tempdir;
+use test_assets::TestAssetDef;
+use test_log::test;
+
+#[test]
+#[cfg(feature = "xz")]
+fn test_replace() {
+    const FILE_NAME: &str = "out.squashfs";
+    let asset_defs = [TestAssetDef {
+        filename: FILE_NAME.to_string(),
+        hash: "6195e4d8d14c63dffa9691d36efa1eda2ee975b476bb95d4a0b59638fd9973cb".to_string(),
+        url: format!("wcampbell.dev/squashfs/testing/test_05/{FILE_NAME}"),
+    }];
+    const TEST_PATH: &str = "test-assets/test_05";
+
+    test_assets::download_test_files(&asset_defs, TEST_PATH, true).unwrap();
+    let image_path = format!("{TEST_PATH}/{FILE_NAME}");
+
+    // extract single file
+    let tmp_dir = tempdir().unwrap();
+    let cmd = Command::cargo_bin("unsquashfs")
+        .unwrap()
+        .env("RUST_LOG", "none")
+        .args([
+            "--path-filter",
+            r#"/b/c/d"#,
+            "-i",
+            &image_path,
+            "-d",
+            tmp_dir.path().join("squashfs-root-rust").to_str().unwrap(),
+        ])
+        .unwrap();
+    cmd.assert().code(0);
+
+    // edit that file
+    let text = b"The mystery of life isn't a problem to solve, but a reality to experience.";
+    std::fs::write(
+        tmp_dir
+            .path()
+            .join("squashfs-root-rust/b/c/d")
+            .to_str()
+            .unwrap(),
+        text,
+    )
+    .unwrap();
+
+    // replace that file
+    let cmd = Command::cargo_bin("replace")
+        .unwrap()
+        .env("RUST_LOG", "none")
+        .args([
+            &image_path,
+            tmp_dir
+                .path()
+                .join("squashfs-root-rust/b/c/d")
+                .to_str()
+                .unwrap(),
+            "/b/c/d",
+            "-o",
+            tmp_dir.path().join("replaced").to_str().unwrap(),
+        ])
+        .unwrap();
+    cmd.assert().code(0);
+
+    // extract
+    let cmd = Command::cargo_bin("unsquashfs")
+        .unwrap()
+        .env("RUST_LOG", "none")
+        .args([
+            "--path-filter",
+            r#"/b/c/d"#,
+            "-i",
+            tmp_dir.path().join("replaced").to_str().unwrap(),
+            "-d",
+            tmp_dir.path().join("squashfs-root-rust2").to_str().unwrap(),
+        ])
+        .unwrap();
+    cmd.assert().code(0);
+
+    // assert the text changed!
+    let bytes = std::fs::read(
+        tmp_dir
+            .path()
+            .join("squashfs-root-rust2/b/c/d")
+            .to_str()
+            .unwrap(),
+    )
+    .unwrap();
+    assert_eq!(bytes, text);
+}

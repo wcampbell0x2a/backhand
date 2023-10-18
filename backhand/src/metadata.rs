c8457ac40ffcfe5cfc24c254c9db4c7347000da0
wcampbell <wcampbell1995@gmail.com>
2023-10-18 00:26:42 -0400

    Use new deku writer

--- backhand/src/metadata.rs
+++ backhand/src/metadata.rs
@@ -1,7 +1,6 @@
 use std::collections::VecDeque;
 use std::io::{self, Cursor, Read, Seek, Write};
 
-use deku::bitvec::BitVec;
 use deku::prelude::*;
 use tracing::trace;
 
@@ -39,6 +38,7 @@ impl MetadataWriter {
     }
 
     fn add_block(&mut self) -> io::Result<()> {
+        trace!("adding block");
         // uncompress data that will create the metablock
         let uncompressed_len = self.uncompressed_bytes.len().min(METADATA_MAXSIZE);
         if uncompressed_len == 0 {
@@ -58,9 +58,11 @@ impl MetadataWriter {
 
         // Remove the data consumed, if the uncompressed data is smalled, use it.
         let (compressed, metadata) = if compressed.len() > uncompressed_len {
+            trace!("using uncompressed");
             let uncompressed = self.uncompressed_bytes.drain(0..uncompressed_len).collect();
             (false, uncompressed)
         } else {
+            trace!("using compressed");
             self.uncompressed_bytes.drain(0..uncompressed_len);
             (true, compressed)
         };
@@ -70,7 +72,6 @@ impl MetadataWriter {
         trace!("new metadata start: {:#02x?}", self.metadata_start);
         self.final_bytes.push((compressed, metadata));
 
-        trace!("LEN: {:02x?}", self.uncompressed_bytes.len());
         Ok(())
     }
 
@@ -81,15 +82,14 @@ impl MetadataWriter {
         }
 
         // write all the metadata blocks
-        for (compressed, cb) in &self.final_bytes {
-            trace!("len: {:02x?}", cb.len());
-            //trace!("total: {:02x?}", out.len());
-            let mut bv = BitVec::new();
+        for (compressed, compressed_bytes) in &self.final_bytes {
+            trace!("len: {:02x?}", compressed_bytes.len());
             // if uncompressed, set the highest bit of len
-            let len = cb.len() as u16 | if *compressed { 0 } else { 1 << (u16::BITS - 1) };
-            len.write(&mut bv, self.kind.inner.data_endian)?;
-            out.write_all(bv.as_raw_slice())?;
-            out.write_all(cb)?;
+            let len =
+                compressed_bytes.len() as u16 | if *compressed { 0 } else { 1 << (u16::BITS - 1) };
+            let mut writer = Writer::new(out);
+            len.to_writer(&mut writer, self.kind.inner.data_endian)?;
+            out.write_all(compressed_bytes)?;
         }
 
         Ok(())

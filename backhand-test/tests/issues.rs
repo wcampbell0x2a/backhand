a711c3eaaffde917ed968bea2eee563aac61b2f3
Rubens Brandao <git@rubens.io>
2023-11-21 16:46:23 -0300

    add test for issue 359

--- backhand-test/tests/issues.rs
+++ backhand-test/tests/issues.rs
@@ -1,8 +1,20 @@
+use backhand::NodeHeader;
+
 /// https://github.com/wcampbell0x2a/backhand/issues/275
 #[test]
 #[cfg(feature = "xz")]
 fn issue_275() {
     let mut writer = std::io::Cursor::new(vec![]);
     let mut fs = backhand::FilesystemWriter::default();
+    fs.write(&mut writer).unwrap();
+}
+
+/// https://github.com/wcampbell0x2a/backhand/issues/359
+#[test]
+fn issue_359() {
+    let mut writer = std::io::Cursor::new(vec![]);
+    let mut fs = backhand::FilesystemWriter::default();
+    let header = NodeHeader { permissions: 0, uid: 1, gid: 2, mtime: 3 };
+    fs.push_dir_all("a/b/c/d/e/f/g", header).unwrap();
     fs.write(&mut writer).unwrap();
 }

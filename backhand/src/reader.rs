373b1be35af9c643e90c611298a3b35b05f09507
wcampbell <wcampbell1995@gmail.com>
2024-05-06 17:43:57 -0400

    backhand: Remove unused SeekRewind

--- backhand/src/reader.rs
+++ backhand/src/reader.rs
@@ -68,29 +68,6 @@ where
     }
 }
 
-/// Similar to to Seek, but only require the `rewind` function
-pub trait SeekRewind {
-    /// Set the IO position back at the start
-    fn rewind(&mut self) -> std::io::Result<()>;
-}
-
-impl<T> SeekRewind for T
-where
-    T: Seek,
-{
-    fn rewind(&mut self) -> std::io::Result<()> {
-        <Self as Seek>::rewind(self)
-    }
-}
-
-/// Pseudo-Trait for Read + SeekRewind
-pub trait ReadRewind: Read + SeekRewind {}
-impl<T: Read + SeekRewind> ReadRewind for T {}
-
-/// Pseudo-Trait for BufRead + SeekRewind
-pub trait BufReadRewind: BufRead + SeekRewind {}
-impl<T: BufRead + SeekRewind> BufReadRewind for T {}
-
 /// Pseudo-Trait for BufRead + Seek
 pub trait BufReadSeek: BufRead + Seek + Send {}
 impl<T: BufRead + Seek + Send> BufReadSeek for T {}

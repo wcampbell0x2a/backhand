f7cb451ca7c9bd94c7596d91376bf0c436da5df3
Wolfyxon <wolfyxon@gmail.com>
2024-11-10 13:14:26 +0100

    Shorter access paths

--- backhand/src/os_traits.rs
+++ backhand/src/os_traits.rs
@@ -1,6 +1,12 @@
 use std::ffi::OsStr;
 use std::ffi::OsString;
 
+#[cfg(unix)]
+use std::os::unix::ffi::OsStrExt as OsStrExtUnix;
+
+#[cfg(unix)]
+use std::os::unix::ffi::OsStringExt as OsStringExtUnix;
+
 pub trait OsStrExt {
     fn as_bytes(&self) -> &[u8];
     fn from_bytes(slice: &[u8]) -> &Self;
@@ -9,11 +15,11 @@ pub trait OsStrExt {
 #[cfg(unix)]
 impl OsStrExt for OsStr {
     fn as_bytes(&self) -> &[u8] {
-        std::os::unix::ffi::OsStrExt::as_bytes(self)
+        OsStrExtUnix::as_bytes(self)
     }
 
     fn from_bytes(slice: &[u8]) -> &Self {
-        std::os::unix::ffi::OsStrExt::from_bytes(slice)
+        OsStrExtUnix::from_bytes(slice)
     }
 }
 
@@ -35,7 +41,7 @@ pub trait OsStringExt {
 #[cfg(unix)]
 impl OsStringExt for OsString {
     fn from_vec(vec: Vec<u8>) -> Self {
-        std::os::unix::ffi::OsStringExt::from_vec(vec)
+        OsStringExtUnix::from_vec(vec)
     }
 }
 

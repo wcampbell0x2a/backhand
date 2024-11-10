1196e6c746b2706fcd2e2e4dcd9ef04f9616db61
Wolfyxon <wolfyxon@gmail.com>
2024-11-10 12:29:10 +0100

    Init from_bytes()

--- backhand/src/os_traits.rs
+++ backhand/src/os_traits.rs
@@ -2,6 +2,7 @@ use std::ffi::OsStr;
 
 pub trait OsStrExt {
     fn as_bytes(&self) -> &[u8];
+    fn from_bytes(slice: &[u8]) -> &Self;
 }
 
 #[cfg(unix)]
@@ -9,11 +10,19 @@ impl OsStrExt for OsStr {
     fn as_bytes(&self) -> &[u8] {
         std::os::unix::ffi::OsStrExt::as_bytes(self)
     }
+
+    fn from_bytes(slice: &[u8]) -> &Self {
+        std::os::unix::ffi::OsStrExt::from_bytes(slice)
+    }
 }
 
 #[cfg(windows)]
 impl OsStrExt for OsStr {
     fn as_bytes(&self) -> &[u8] {
         self.to_string_lossy().as_bytes()
+    }
+
+    fn from_bytes(slice: &[u8]) -> &Self {
+        todo!()
     }
 }

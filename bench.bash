9f0e924ee42cdfb31e0464fd5c559a2a1b397b4f
wcampbell <wcampbell1995@gmail.com>
2026-08-01 11:44:59 -0400

    Remove inaccurate comment

--- bench.bash
+++ bench.bash
@@ -11,7 +11,6 @@ BACKHAND="./target/dist/unsquashfs-backhand"
 BACKHAND_MUSL="./target/x86_64-unknown-linux-musl/dist/unsquashfs-backhand"
 UNSQUASHFS="/usr/bin/unsquashfs"
 
-# Using dynamic linked xz for perf reasons and matching unsquashfs in this testing
 FLAGS="--bins --locked --profile=dist --no-default-features --features xz --features zstd --features gzip --features parallel"
 
 bench () {
